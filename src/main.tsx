7e40c9a73cedf223ca1cb7ff1329195b55c537f8
BenjamimEterino <benjamimtoretto901@gmail.com>
2023-07-13 12:22:16 +0200

    Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Rotas } from './router.tsx';
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
